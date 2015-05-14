c51605d579fb481a1f3699b4a75dfd04a8b6d758
end3rs-army <eric.m.dowty@gmail.com>
2015-05-14 12:28:45 -0600

    landing page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources    :cart, only: [:index, :create, :update]
 
   get        '/home', to: 'home#new'
+  get       '/about', to: 'home#about'
   get       '/login', to: 'session#new'
   post      '/login', to: 'session#create'
   delete   '/logout', to: 'session#destroy'
