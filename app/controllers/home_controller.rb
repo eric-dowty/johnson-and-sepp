9b2be13c1a34f7ec18d220e98970bf8d2063eca0
end3rs-army <eric.m.dowty@gmail.com>
2015-05-14 14:48:06 -0600

    updated structure page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,4 +3,10 @@ class HomeController < ApplicationController
   def new
   end
 
+  def about
+  end
+
+  def structure
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   get        '/home', to: 'home#new'
   get       '/about', to: 'home#about'
+  get   '/structure', to: 'home#structure'
   get       '/login', to: 'session#new'
   post      '/login', to: 'session#create'
   delete   '/logout', to: 'session#destroy'
